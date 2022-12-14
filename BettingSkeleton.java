400a89a8009e060d6a3e1959dcb18c5f34602442
MaxGoldie <113855541+MaxGoldie@users.noreply.github.com>
2022-12-14 17:39:41 -0500

    Create BettingSkeleton.java

---
+++ BettingSkeleton.java
@@ -0,0 +1,32 @@
+
+public class BettingSkeleton {
+
+	public BettingSkeleton(int minBalance, IRandomValueGenerator random) {
+
+	}
+
+	public double getCurrentBalance() {
+		return 0;
+
+	}
+
+	public boolean canBet(double amnt) {
+		return false;
+
+	}
+
+	public void addMoney(double amnt) {
+
+	}
+
+	public double betOnANumber(double amnt, int min, int max, int selectedNumber) {
+		return 0;
+
+	}
+
+	public double betOnProbability(double amnt, double p) {
+		return 0;
+
+	}
+
+}
